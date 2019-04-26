ce04fc0f46ad4b33b3cfb3b16eef251001c8c139
Caitlyn Greffly <cgreffly@gmail.com>
2019-04-26 08:45:14 -0700

    add js file

---
+++ script.js
@@ -0,0 +1,27 @@
+function handleIconClick() {
+  $('#phoneNumber').on('click', function() {
+    $('.displayInfo').html(`<h2>(707) 529-8295</h2>`);
+  });
+  $('#email').on('click', function() {
+    $('.displayInfo').html(`<h2>cgreffly@gmail.com</h2>`);
+  });
+}
+
+$("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
+
+/*function handleNavBar() {
+  $('#home').on('click').scrollto('.projects');
+}*/
+
+function handlePage() {
+  handleIconClick();
+  //handleNavBar();
+}
+
+$(handlePage);
