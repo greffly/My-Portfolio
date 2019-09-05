bc03fe4ff48b34152d3ae0da2cfc2677db57179a
Caitlyn <cgreffly@gmail.com>
2019-09-05 08:17:31 -0700

    add hamburger menu

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const menuButton = document.querySelector('.menuButton');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.navigationBar');
+const navItems = document.querySelectorAll('.navItem');
+
+let showMenu = false;
+menuButton.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuButton.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+    showMenu = true;
+  } else {
+    menuButton.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+    showMenu = false;
+  }
+}
+
 function handleIconClick() {
   $('#email').on('click', function() {
     $('.displayInfo').html(`<h2>cgreffly@gmail.com</h2>`);
@@ -7,7 +31,11 @@ function handleIconClick() {
 $('nav')
   .find('.pageLink')
   .click(function(e) {
-    e.preventDefault();
+    menuButton.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+    showMenu = false;
     let section = $(this).attr('href');
     $('html, body').animate({
       scrollTop: $(section).offset().top
